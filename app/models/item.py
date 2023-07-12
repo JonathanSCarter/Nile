df07324178fa1bfdc35f8ef68c3be441366b5262
JonathanSCarter <jscarter3874112@gmail.com>
2023-07-12 11:52:36 -0400

    setup done

---
+++ app/models/item.py
@@ -0,0 +1,17 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Item(db.Model):
+  __tablename__ = 'items'
+
+  if environment == "production":
+    __table_args__ = {'schema': SCHEMA}
+
+  id = db.Column(db.Integer, primary_key=True)
+  seller = db.Column(db.String(40))
+  rating = db.Column(db.Float(2))
+  price = db.Column(db.Float(7))
+  name = db.Column(db.String(200))
+  discount = db.Column(db.Integer)
+  img = db.Column(db.String(400))
+  description = db.Column(db.String(1000))
+  category = db.Column(db.String(100))
--- app/models/user.py
+++ app/models/user.py
@@ -1,33 +1,35 @@
-from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-
+from .db import db, environment, SCHEMA
 
 class User(db.Model, UserMixin):
-    __tablename__ = 'users'
+  __tablename__ = 'users'
 
-    if environment == "production":
-        __table_args__ = {'schema': SCHEMA}
+  if environment == "production":
+    __table_args__ = {'schema': SCHEMA}
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
-    email = db.Column(db.String(255), nullable=False, unique=True)
-    hashed_password = db.Column(db.String(255), nullable=False)
+  id = db.Column(db.Integer, primary_key=True)
+  first_name = db.Column(db.String(40), nullable=False)
+  last_name = db.Column(db.String(40), nullable=False)
+  email = db.Column(db.String(255), nullable=False, unique=True)
+  hashed_password = db.Column(db.String(255), nullable=False)
 
-    @property
-    def password(self):
-        return self.hashed_password
+  cart = db.relationship("Cart", back_populates="user")
 
-    @password.setter
-    def password(self, password):
-        self.hashed_password = generate_password_hash(password)
+  @property
+  def password(self):
+    return self.hashed_password
 
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
+  @password.setter
+  def password(self, password):
+    self.hashed_password = generate_password_hash(password)
 
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'username': self.username,
-            'email': self.email
-        }
+  def check_password(self, password):
+    return check_password_hash(self.password, password)
+
+  def to_dict(self):
+    return {
+      'id': self.id,
+      'username': self.username,
+      'email': self.email
+    }
