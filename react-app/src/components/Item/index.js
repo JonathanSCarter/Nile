99128bc42e54077ed634b287cb0645f4c0a7c9d6
JonathanSCarter <jscarter3874112@gmail.com>
2023-07-20 13:55:00 -0400

    more css done

--- react-app/src/components/Item/index.js
+++ react-app/src/components/Item/index.js
@@ -1,27 +1,35 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { thunkGetItem } from "../../store/item";
-import { useHistory } from 'react-router-dom';
-import './item.css';
-function Item({item}){
+import { useHistory } from "react-router-dom";
+import "./item.css";
+function Item({ item }) {
   const history = useHistory();
   const dispatch = useDispatch();
 
   const handleSelect = () => {
-    dispatch(thunkGetItem(item.id))
-    .then(() => history.push(`/items/${item.id}`));
-  }
+    dispatch(thunkGetItem(item.id)).then(() =>
+      history.push(`/items/${item.id}`)
+    );
+  };
 
-  return(
-    <div className='item-container' onClick={handleSelect}>
+  return (
+    <div className="item-container" onClick={handleSelect}>
       <img src={item.image} alt="Item Image" />
-      <div>
-      <p>{item.name}</p>
-      <p>{item.rating} star</p>
-      <p>{item.price.toFixed(2)}</p>
+      <div className="item-details">
+        <p>{item.name}</p>
+        <p>{item.rating} star</p>
+        <span className="price-style">
+        <p style={item.discount > 0 ? { textDecoration: "line-through", marginRight:"8px" } : {}}>
+          {item.price.toFixed(2)}
+        </p>
+        {item.discount > 0 && (
+          <p> On sale for {(item.price - item.price * (item.discount / 100)).toFixed(2)}! {item.discount}% off!</p>
+          )}
+          </span>
       </div>
     </div>
-  )
+  );
 }
 
-export default Item
+export default Item;
