97505877d11e416fc9c25545bb1c61e9c8b78c5d
JonathanSCarter <jscarter3874112@gmail.com>
2023-08-11 14:45:42 -0400

    fixed date string

--- react-app/src/components/OldCartItem/index.js
+++ react-app/src/components/OldCartItem/index.js
@@ -1,9 +1,16 @@
 import React, {useEffect, useState} from 'react';
 import './OldCartItem.css'
 function OldCartItem({cart}){
+  const purchasedDate = new Date(cart.purchased_at);
+
+  const formattedDate = purchasedDate.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
   return (
     <div className='order-history-item'>
-    <h2>You bought {cart.count} {cart.item_name}{cart.item_name[-1] === 's' ? cart.count > 1 ? "'s" : null : cart.count > 1 ? 's' : null} on {cart.purchased_at}</h2>
+    <h2>You bought {cart.count} {cart.item_name}{cart.item_name[-1] === 's' ? cart.count > 1 ? "'s" : null : cart.count > 1 ? 's' : null} on {formattedDate}</h2>
     </div>
   )
 }
